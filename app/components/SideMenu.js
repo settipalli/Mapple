89731fc7abb00c7c559d757195516dc79ef6bfdf
Santhoshkumar Settipalli <santhosh.settipalli@gmail.com>
2017-08-23 06:06:12 +0530

    Added List and ListItems that represent our Menu in the SideMenu.js.

--- app/components/SideMenu.js
+++ app/components/SideMenu.js
@@ -12,10 +12,43 @@ import {
 
 export default class SideMenu extends Component {
   constructor(props) {
-    super(props )
+    super(props)
   }
-  
+
   render() {
-    return null 
+    // array of objects - each of those objects would be the menu items of our side-menu
+    // each object has two pices, title: menu-item-name and an event-hander like onPress
+    let list = [
+      {
+        title: 'Match',
+        onPress: () => {
+          // we will use the 'navigator' passed in as properties
+          // we use 'replace' in-place of push because we want 'Match' to be a root scene and we do not want
+          // the user to be able to navigate backwards using the 'arrow' on the top-left
+          this.props.navigator.replace("Match")   // 'Match' is the title of the scene that we want to render
+        }
+      },
+      {
+        title: 'History',
+        onPress: () => {
+          this.props.navigator.replace("History")
+        }
+      }
+    ]
+
+    return (
+      <Container theme={this.props.theme}>
+        <Header />
+        <View>
+          <List dataArray={list}
+                renderRow={
+                  (item) => <ListItem button onPress={item.onPress.bind(this)}>
+                              <Text> {item.title} </Text>
+                            </ListItem>
+                }
+          />
+        </View>
+      </Container>
+    )
   }
 }
