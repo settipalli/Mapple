7a1415caf0199599a2fced6aed548c5ee853b7e3
Santhoshkumar Settipalli <santhosh.settipalli@gmail.com>
2017-08-23 07:07:35 +0530

    Added teh first mobx-firebase store to represent app settings. Also added the login and splash images.

---
+++ app/stores/SettingsStore.js
@@ -0,0 +1,28 @@
+import firebase from 'firebase'
+import MobxFirebaseStore from 'mobx-firebase-store'
+
+import FirebaseConfig from '../configs/firebase.config'
+
+// let us create our first mobx-firebase store
+export default class SettingsStore extends MobxFirebaseStore {
+  // one of the foremost responsibilities of this store is to initialize our firebase-sdk
+  constructor() {
+    firebase.initializeApp(FirebaseConfig)
+    
+    // initialize the mobx-firebase store that we inherited from, pass in the reference to our firebase
+    super(firebase.database().ref())
+
+    // now let us create our SettingsStore data that we would like to pass around to other parts of our application
+    this.SplashTime = 5000  // 5000 milliseconds = 5 seconds
+    this.SplashImg = require('../../images/Splash.jpg')
+  }
+
+  // accessors
+  get SplashTime() {
+    return this.SplashTime
+  }
+
+  get SplashImg() {
+    return this.SplashImg
+  }
+}
---
+++ images/README.md
@@ -0,0 +1,11 @@
+# Image Credits
+
+1. Splash.jpg
+  - Source: https://pixabay.com/en/breakfast-idli-indian-foods-2408818/
+  - CC0 Public Domain
+  - Free for commercial use
+
+2. Login.jpg
+  - Source: http://maxpixel.freegreatpicture.com/Tasty-Wooden-Background-Food-Olives-Background-2068215
+  - CC0 Public Domain
+  - Free for commercial use
