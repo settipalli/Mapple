5145457a33013ec1fcfb2dc5a73201bfaa13aa8a
Santhoshkumar Settipalli <santhosh.settipalli@gmail.com>
2017-08-23 05:53:23 +0530

    Added a 'components' directory within the 'app' directory. Also created a skeleton SideMenu component.

---
+++ app/components/SideMenu.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import {
+  Container,    // over-arching container component for our view
+  Header,       // wraps the JSX for the header portion of our side-menu
+  Content,      // holds the content of the side-menu
+  List,         // represents a list which holds the menu items that are listed as list-items within it
+  ListItem,     // items inside the List
+  Text,         // any-text
+  View,
+  Button
+} from 'native-base'
+
+export default class SideMenu extends Component {
+  constructor(props) {
+    super(props )
+  }
+  
+  render() {
+    return null 
+  }
+}
